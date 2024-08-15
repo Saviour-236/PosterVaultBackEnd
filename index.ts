0eec6b8b7cc52607b0b3428aeaa4a2f6ce7f0f5a
Suresh Kumar <thakursureshkumar118@gmail.com>
2024-08-15 08:56:11 +0530

    try4

--- index.ts
+++ index.ts
@@ -16,33 +16,15 @@ import authentcationRoutes from './routes/authenticationRoutes';
 const app = express();
 
 // Middleware
-// app.use(cors({
-//     origin: [
-//         'http://localhost:5173',            // Local development
-//         'https://texotiles.netlify.app'     // Production front-end
-//     ],
-//     credentials: true,                     // Allow credentials (cookies, headers, etc.)
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-// }));
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        "https://texotiles.netlify.app"
+    ],
+    credentials: true ,// Allow credentials (cookies, authorization headers)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
-app.use((req, res, next) => {
-    const allowedOrigins = ['http://localhost:5173', 'https://texotiles.netlify.app'];
-    const origin = req.headers.origin;
-    
-    if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
-    }
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    
-    // Handle preflight requests
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-});
 
 app.use(cookieParser());
 app.use(express.json());
