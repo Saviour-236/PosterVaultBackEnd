de7b372d992472d73eb3b6a8732a39f6541bbd39
Suresh Kumar <thakursureshkumar118@gmail.com>
2024-08-17 12:19:05 +0530

    try again

--- middleware/authorization.ts
+++ middleware/authorization.ts
@@ -1,32 +1,34 @@
 // middleware that authorize the routes according to the user role on behalf of their token 
 import type { Request, Response, NextFunction } from 'express';
-import jwt, {  type JwtPayload } from 'jsonwebtoken';
+import jwt, { type JwtPayload } from 'jsonwebtoken';
 const SECRET_KEY = process.env.AUTH_TOKEN_SECRET_KEY as string;
 // middleware that authorize the routes according to the user role on behalf of their token
 const authorization = (req: Request, res: Response, next: NextFunction) => {
     console.log('in authorization middleware');
-    console.log("cookies",req.cookies);
+    console.log("cookies", req.cookies);
     // get token from cookies
     const token = req.cookies.authtoken;
-console.log("this is token" ,token)
+    console.log("this is token", token)
     //checking if token exist in request  cookies or not
     if (!token) {
         console.log('no token found in cookies');
-        return res.status(401).json({message:'not authorized please sign in first '});
+        return res.status(401).json({ message: 'not authorized please sign in first ' });
     }
 
     // checking user is admin or not with decoding the token and also checking if token is valid or not
-    jwt.verify(token, SECRET_KEY,(err: jwt.VerifyErrors | any, decodedUser: JwtPayload | any ) => {
+    jwt.verify(token, SECRET_KEY, (err: jwt.VerifyErrors | any, decodedUser: JwtPayload | any) => {
         if (err) {
             return res.status(401).clearCookie(req.cookies.authtoken).json({ name: err.name, message: err.message })
         }
         const { admin } = decodedUser.user;
+        console.log('this is admin', admin);
         if (!admin) {
             return res.status(401).json({ message: ' only admin can access this route ' })
         }
-        next();
-    })
+        })
+    console.log('user is admin just before next');
 
+    next();
 }
 
 export default authorization;
