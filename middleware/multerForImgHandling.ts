6a131a18bf550295a9bd5a47dfaab0c374812d00
Suresh Kumar <thakursureshkumar118@gmail.com>
2024-08-17 12:58:16 +0530

    still trying with chagin in multer file

--- middleware/multerForImgHandling.ts
+++ middleware/multerForImgHandling.ts
@@ -1,19 +1,19 @@
 // multer config file
 import multer from 'multer';
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      console.log('in multer destination');
-      cb(null, '../public/uploads/'); // Temporary storage directory
-    },
-    filename: function (req, file, cb) {
-      console.log('in multer destination');
-      console.log(file);
-      console.log(file.originalname);
-      cb(null, file.originalname); // Use original filename
-
-    }
-  });
- const upload = multer({ storage });
- //const upload = multer({ dest: './public/data/uploads/' })
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       console.log('in multer destination');
+//       cb(null, '../public/uploads/'); // Temporary storage directory
+//     },
+//     filename: function (req, file, cb) {
+//       console.log('file name starting here');
+//       console.log(file);
+//       console.log(file.originalname);
+//       cb(null, file.originalname); // Use original filename
+// console.log('file name ending here');
+//     }
+//   });
+//  const upload = multer({ storage });
+const upload = multer({ dest: './public/data/uploads/' })
 
 export default upload;
