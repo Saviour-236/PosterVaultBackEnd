f4c6c5da799e066195bdc4b187f308e0d5abe27d
Suresh Kumar <thakursureshkumar118@gmail.com>
2026-07-08 11:54:05 +0530

    Cors error fixed for new vercel deployment

--- index.ts
+++ index.ts
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://poster-vault-project-1.netlify.app'],
+    origin: ['http://localhost:5173', 'https://poster-vault-project-1.netlify.app','https://poster-vault-front-end-4el1-five.vercel.app'],
     credentials: true, // Allow credentials (cookies, authorization headers)
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   //  allowedHeaders: ['Content-Type', 'Authorization'], // Add any other headers you might need
